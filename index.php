19ad90c4408723c0d13cf90ea4222e8695a7e1fa
Christopher Milne <christopher.milne@gmail.com>
2013-02-04 09:02:09 -0500

    convert all folder names to lowercase, and adjust links

--- index.php
+++ index.php
@@ -7,10 +7,10 @@
 <body>
 	<nav>
 		<ul>
-			<li><a href="Chapter1-Getting-Started-with-PHP/index.php">Chapter1-Getting-Started-with-PHP</a></li>
-			<li><a href="Chapter2-Variables/index.php">Chapter2-Variables</a></li>
-			<li><a href="Chapter3-HTML-Forms-and-PHP/index.php">Chapter3-HTML-Forms-and-PHP</a></li>
-			<li><a href="Chapter4-Using-Numbrers/index.php">Chapter4-Using-Numbrers</a></li>
+			<li><a href="chapter1-getting-started-with-php/index.php">Chapter1-Getting-Started-with-PHP</a></li>
+			<li><a href="chapter2-variables/index.php">Chapter2-Variables</a></li>
+			<li><a href="chapter3-html-forms-and-php/index.php">Chapter3-HTML-Forms-and-PHP</a></li>
+			<li><a href="chapter4-using-numbrers/index.php">Chapter4-Using-Numbrers</a></li>
 		</ul>
 	</nav>
 </body>
