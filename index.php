749ed18d179ba1e671f6125317b8a08c28077f5e
Christopher Milne <christopher.milne@gmail.com>
2013-02-04 08:58:12 -0500

    set intro page and exercise folders

---
+++ index.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Php Self Study – Review and Pursue</title>
+</head>
+<body>
+	<nav>
+		<ul>
+			<li><a href="Chapter1-Getting-Started-with-PHP/index.php">Chapter1-Getting-Started-with-PHP</a></li>
+			<li><a href="Chapter2-Variables/index.php">Chapter2-Variables</a></li>
+			<li><a href="Chapter3-HTML-Forms-and-PHP/index.php">Chapter3-HTML-Forms-and-PHP</a></li>
+			<li><a href="Chapter4-Using-Numbrers/index.php">Chapter4-Using-Numbrers</a></li>
+		</ul>
+	</nav>
+</body>
+</html>
